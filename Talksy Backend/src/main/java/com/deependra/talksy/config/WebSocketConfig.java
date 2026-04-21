19513aa910b0f6aea8d1365cfcb0182fa329c56c
Deependra Kumar <deependraraj2109@gmail.com>
2026-04-22 00:11:02 +0530

    Fix allowed origin patterns in WebSocketConfig

--- Talksy Backend/src/main/java/com/deependra/talksy/config/WebSocketConfig.java
+++ Talksy Backend/src/main/java/com/deependra/talksy/config/WebSocketConfig.java
@@ -36,7 +36,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOriginPatterns("http://localhost:*", "http://127.0.0.1:*", "null","https://*.netlify.app"")
+                .setAllowedOriginPatterns("http://localhost:*", "http://127.0.0.1:*", "null","https://*.netlify.app")
                 .withSockJS();
     }
 
